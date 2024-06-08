386f9c5bffb12d674cc301cbf6942352985f8060
nitzan <x@x.com>
2024-06-08 19:53:55 +0300

    targil

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 export function App() {
   return (
-    // 1. Show a list all the user names
-    // 2. When clicking on user, show its first post
-    // 3. Add a search for users
-    // 4. Seperate to components
-    // 5. Seperate to hooks
-    // 6. style
     <div>
       hello react
     </div>
